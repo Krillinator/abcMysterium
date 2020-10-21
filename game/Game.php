1ad8927ffc830927b7833610b1cbffd3c0800e28
kristoffer <kristoffer9464@hotmail.com>
2020-10-21 14:41:27 +0200

    Finished the protoype

--- game/Game.php
+++ game/Game.php
@@ -9,13 +9,28 @@ class Game extends MysteryGame
 {
   public function generateMystery()
   {
-    // Change this expression
+    $letters = range("A", "Z");
+    $answer = [];
+    $mystery = [];
+
+    shuffle($letters);
+
+    // answer = 9 first letters 
+    for ($i = 0; $i < 10; $i++) {
+      $answer[] = $letters[$i];
+    }
+
+    $mystery = $answer; // 9 first letters
+
+    shuffle($mystery); // shuffle mystery - 9 letters
+    sort($answer);
+
+    $mystery_implode = implode("", $mystery);
+    $answer_implode = implode("", $answer);
+
     $this->mystery = [
-      "mystery" => "test",
-      "answer" => "test"
+      "mystery" => $mystery_implode,
+      "answer" => $answer_implode
     ];
   }
 }
-
-
-?>
